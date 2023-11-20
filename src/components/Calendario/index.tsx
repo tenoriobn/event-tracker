d9a001e5a73d9e302924b52c2af9875e8640d894
Bruno Tenório <tenoriobn@gmail.com>
2023-11-20 17:06:30 -0300

    Criando atom para ListaDeEventosState

--- src/components/Calendario/index.tsx
+++ src/components/Calendario/index.tsx
@@ -1,6 +1,6 @@
-
 import React from 'react'
-import { IEvento } from '../../interfaces/IEvento';
+import { useRecoilValue } from 'recoil';
+import { listaDeEventosState } from '../../state/atom';
 import style from './Calendario.module.scss';
 import ptBR from './localizacao/ptBR.json'
 import Kalend, { CalendarView } from 'kalend'
@@ -14,9 +14,10 @@ interface IKalendEvento {
   color: string
 }
 
-const Calendario: React.FC<{ eventos: IEvento[] }> = ({ eventos }) => {
+const Calendario: React.FC = () => {
 
   const eventosKalend = new Map<string, IKalendEvento[]>();
+  const eventos = useRecoilValue(listaDeEventosState);
 
   eventos.forEach(evento => {
     const chave = evento.inicio.toISOString().slice(0, 10)
