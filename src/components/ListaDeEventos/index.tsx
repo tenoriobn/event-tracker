75ab4432615e04f13c2dfdf638485cbc6b34bd05
Bruno Tenório <tenoriobn@gmail.com>
2023-11-29 12:50:27 -0300

    Lista de eventos via hook

--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { useRecoilValue } from 'recoil';
-import { listaDeEventosState } from '../../state/atom';
 import Evento from '../Evento';
 import Filtro from '../Filtro';
 import style from './ListaDeEventos.module.scss';
+import useListaDeEventos from '../../state/hooks/useListaDeEventos';
 
 const ListaDeEventos: React.FC<{ 
   aoFiltroAplicado: (data: Date | null) => void }> = ({ aoFiltroAplicado }) => {
 
-  const eventos = useRecoilValue(listaDeEventosState);
+  const eventos = useListaDeEventos();
 
   return (<section>
     <Filtro aoFiltroAplicado={aoFiltroAplicado} />
